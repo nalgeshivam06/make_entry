6bf96001b8e6cdbcb3c5bddda6f1a11d35f375b0
Shivam Nalge <shivamnalge0610@gmail.com>
2024-01-15 11:21:04 +0530

    rechecked

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -20,7 +20,7 @@ const InvoiceApp = ({ invoices, addInvoice, updateInvoice, deleteInvoice }) => {
   const handleAddInvoice = () => {
 
     addInvoice({
-      id: Date.now(), 
+      id: Date.now(),
       ...formData,
     });
 
@@ -46,62 +46,62 @@ const InvoiceApp = ({ invoices, addInvoice, updateInvoice, deleteInvoice }) => {
       alert("Deletion Cancelled");
     }
   };
-  
+
 
   return (
     <div className={styles.container}>
       <div className="heading"><h2>Invoice App</h2></div>
       <div>
         <div className={styles.lebel_container}>
-        <label className={styles.page_label}>Name</label>
-        <label className={styles.page_label}>Amount</label>
-        <label className={styles.page_label}>Status</label>
+          <label className={styles.page_label}>Name</label>
+          <label className={styles.page_label}>Amount</label>
+          <label className={styles.page_label}>Status</label>
 
         </div>
         <div className={styles.input_container}>
-    
-        <input className={styles.detail_input} type="text" name="name" value={formData.name} onChange={handleInputChange} />
-        <input className={styles.detail_input} type="text" name="paymentAmount" value={formData.paymentAmount} onChange={handleInputChange} />
-        <input className={styles.detail_input} type="text" name="status" value={formData.status} onChange={handleInputChange} />
 
-        <button className={styles.invoice_button} onClick={handleAddInvoice}>Add New Invoice</button>
-     
-      </div>
+          <input className={styles.detail_input} type="text" name="name" value={formData.name} onChange={handleInputChange} />
+          <input className={styles.detail_input} type="text" name="paymentAmount" value={formData.paymentAmount} onChange={handleInputChange} />
+          <input className={styles.detail_input} type="text" name="status" value={formData.status} onChange={handleInputChange} />
+
+          <button className={styles.invoice_button} onClick={handleAddInvoice}>Add New Invoice</button>
+
+        </div>
       </div >
 
       <div className={styles.invoice_list}>
-     
-      
-      <ul className='invoice-list'>
-  {invoices.map((invoice) => (
-    <li key={invoice.id} className={styles.invoice_items}>
-      <div className='invoice-details'>
-        <span className={`${styles.label} ${styles.s3}`}>Name:</span>
-        <span className={`${styles.value} ${styles.s3}`}>{invoice.name}</span>
-        <span className={`${styles.label} ${styles.s3}`}>Amount:</span>
-        <span className={`${styles.value} ${styles.s3}`}>{invoice.paymentAmount}</span>
-        <span className={`${styles.label} ${styles.s3}`}>Status:</span>
-        <span className={`${styles.value} ${styles.s3}`}>{invoice.status}</span>
-      </div>
-      <div className='invoice-buttons'>
-        <button className={styles.invoice_list_button}onClick={() => handleUpdateInvoice(invoice.id)}>Update</button>
-        <button className={`${styles.invoice_list_button} ${styles.delete}`} onClick={() => handleDeleteInvoice(invoice.id)}>Delete</button>
-      </div>
-    </li>
-  ))}
-</ul>
 
-         
+
+        <ul className='invoice-list'>
+          {invoices.map((invoice) => (
+            <li key={invoice.id} className={styles.invoice_items}>
+              <div className='invoice-details'>
+                <span className={`${styles.label} ${styles.s3}`}>Name:</span>
+                <span className={`${styles.value} ${styles.s3}`}>{invoice.name}</span>
+                <span className={`${styles.label} ${styles.s3}`}>Amount:</span>
+                <span className={`${styles.value} ${styles.s3}`}>{invoice.paymentAmount}</span>
+                <span className={`${styles.label} ${styles.s3}`}>Status:</span>
+                <span className={`${styles.value} ${styles.s3}`}>{invoice.status}</span>
+              </div>
+              <div className='invoice-buttons'>
+                <button className={styles.invoice_list_button} onClick={() => handleUpdateInvoice(invoice.id)}>Update</button>
+                <button className={`${styles.invoice_list_button} ${styles.delete}`} onClick={() => handleDeleteInvoice(invoice.id)}>Delete</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+
       </div>
     </div>
   );
-};
+    };
 
-const mapStateToProps = (state) => ({
-  invoices: state.invoices.invoices,
-});
+      const mapStateToProps = (state) => ({
+        invoices: state.invoices.invoices,
+      });
 
-export default connect(mapStateToProps, { addInvoice, updateInvoice, deleteInvoice })(InvoiceApp);
+      export default connect(mapStateToProps, { addInvoice, updateInvoice, deleteInvoice })(InvoiceApp);
 
 
 
